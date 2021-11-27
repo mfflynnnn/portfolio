9e90f521657dca0a8808d0d62ee05a3ef672b624
mfflynnnn <yougotanemailfrommolly@gmail.com>
2021-11-27 14:03:49 -0800

    Edited footer, certs, and, projects
    Added Twitter icon on footer. Added new React certification to Certs page. Added newest project (React Dictionary App) to Projects page. Other minor tweaks.

---
+++ images/cool-visvesvaraya-phx46 - CodeSandbox_files/12.52040911c.chunk.js
@@ -0,0 +1,2 @@
+(this.csbJsonP=this.csbJsonP||[]).push([[12],{"../../node_modules/worker-loader/dist/cjs.js?publicPath=/&name=client-ext-host-worker.[hash:8].worker.js!./src/app/overmind/effects/vscode/extensionHostWorker/bootstrappers/client-ext-host.ts":function(e,s){e.exports=function(){return new Worker("/client-ext-host-worker.04c41d58.worker.js")}}}]);
+//# sourceMappingURL=12.52040911c.chunk.js.map
